902ea17c2e99d4e321fa68c85392ab9b83a586e9
sunbas2 <sunbas777@gmail.com>
2018-06-10 16:42:18 -0400

    new files added

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,15 @@
+// About Me, Portfolio, Contact button//
+
+function() {
+    $("btn1").click(function(){
+
+    });
+    $("btn2")
+    $("btn3")
+};
+
+
+<a href="C:/Users/Max Power/Desktop/CBC/Assignment 8/UpdatedPortfolio/index.html> </a>
+
+
+<a href="UpdatedPortfolio/portfolio.html> </a>
